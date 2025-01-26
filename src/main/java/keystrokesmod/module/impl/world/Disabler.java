0ea72eb195f6e9761680dc461c2f35b10efd2e82
Keg097 <davidchil2007@gmail.com>
2025-01-26 22:17:56 +0000

    added sprint cancel to disabler

--- src/main/java/keystrokesmod/module/impl/world/Disabler.java
+++ src/main/java/keystrokesmod/module/impl/world/Disabler.java
@@ -2,10 +2,12 @@ package keystrokesmod.module.impl.world;
 
 import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.SliderSetting;
+import net.minecraft.network.play.client.C0BPacketEntityAction;
+import net.minecraft.network.play.client.C0BPacketEntityAction.Action;
 
 public class Disabler extends Module {
     private SliderSetting mode;
-    private String[] modes = new String[]{"Meow", "Full", "00"};
+    private String[] modes = new String[]{"Meow", "Full", "00", "SprintCancel"};
 
     public Disabler() {
         super("Disabler", Module.category.world);
@@ -15,5 +17,12 @@ public class Disabler extends Module {
     @Override
     public String getInfo() {
         return modes[(int) mode.getInput()];
+    }
+
+    @Override
+    public void onUpdate() {
+        if (modes[(int) mode.getInput()].equals("SprintCancel")) {
+            mc.thePlayer.sendQueue.addToSendQueue(new C0BPacketEntityAction(mc.thePlayer, Action.STOP_SPRINTING));
+        }
     }
 }
